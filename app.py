4cd57c062f7ce7226ec0cc211d0fa3842b009b7c
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2021-12-27 17:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 from flask.globals import request
-import smtplib  # python library to send emails
-
+# import smtplib  # python library to send emails
+from flask_mail import Mail, Message
 # recipient = input("Enter Email of the Recipient:\n") #receives mail address
 def SendEmail(recipient,amt,name):
 #     server = smtplib.SMTP("smtp.gmail.com" , 587)  # 587 = port number
@@ -23,7 +23,25 @@ def SendEmail(recipient,amt,name):
         
 
 app = Flask(__name__,template_folder='Template')
-
+mail = Mail(app)
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'demo24062000@gmail.com'
+app.config['MAIL_PASSWORD'] = 'kvhebbal'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+def SendEmailq(recipient,amt,name):
+    msg = Message(
+				'BILL AMOUNT',
+				sender ='demo24062000@gmail.com',
+				recipients = [recipient]
+			)
+    SUBJECT = ""
+    amt1=str(amt)
+    msg.body = "Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+amt1+" for these many amout of consumption.\n\tYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa"
+    mail.send(msg)
 @app.route("/")
 def billfeed():
     return render_template('bill.html')
@@ -35,7 +53,7 @@ def bill():
     email=request.form['Email']
     hr=request.form['Hrs']
     amt=rate*float(hr)
-    SendEmail(email,amt,name)
+    SendEmailq(email,amt,name)
     # l={'name':name,'email':email,'hr':hr}
     return render_template('billout.html',name1=name,email1=email,hr1=hr)
     
