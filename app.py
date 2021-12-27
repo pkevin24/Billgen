f70cd9d1bc827756064779f0512fe98b2aee69a6
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2021-12-27 18:54:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 from flask import Flask,render_template,request
 from flask.globals import request
 # import smtplib  # python library to send emails
+# from flask import Flask
 from flask_mail import Mail, Message
+
 # recipient = input("Enter Email of the Recipient:\n") #receives mail address
+def SendEmail(rec,a1,name1):
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    me = "demo24062000@gmail.com"
+    my_password = r"kvhebbal"
+    you = rec
+    name=name1
+    amt1=str(a1)
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Bill Amount"
+    msg['From'] = me
+    msg['To'] = you
+    m1="Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+str(amt1)+" for these many amout of consumption.\nYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa"
+    part2 = MIMEText(m1)
+    
+    msg.attach(part2)
+
+    # Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+    s = smtplib.SMTP_SSL('smtp.gmail.com')
+    
+    s.login(me, my_password)
+
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
 app = Flask(__name__,template_folder='Template')
-mail = Mail(app)
-# configuration of mail
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'demo24062000@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kvhebbal'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
-def SendEmailq(recipient,amt,name):
-    msg = Message(
-				'BILL AMOUNT',
-				sender ='demo24062000@gmail.com',
-				recipients = [recipient]
-			)
-    SUBJECT = ""
-    amt1=str(amt)
-    msg.body = "Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+amt1+" for these many amout of consumption.\n\tYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa"
-    mail.send(msg)
 @app.route("/")
 def billfeed():
     return render_template('bill.html')
@@ -34,7 +44,7 @@ def bill():
     email=request.form['Email']
     hr=request.form['Hrs']
     amt=rate*float(hr)
-#     SendEmailq(email,amt,name)
+    SendEmail(email,amt,name)
     # l={'name':name,'email':email,'hr':hr}
     return render_template('billout.html',name1=name,email1=email,hr1=hr)
     
