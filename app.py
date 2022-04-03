7ee875551632b582f61c8eec638812f70eac36af
pkevin24 <70275725+pkevin24@users.noreply.github.com>
2022-04-03 20:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
 from flask import Flask,render_template,request
 from flask.globals import request
-# import smtplib  # python library to send emails
-from flask_mail import Mail, Message
+import smtplib,ssl  # python library to send emails
+
 # recipient = input("Enter Email of the Recipient:\n") #receives mail address
-app = Flask(__name__,template_folder='Template')
-mail = Mail(app)
-# configuration of mail
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 993
-app.config['MAIL_USERNAME'] = 'demo24062000@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kvhebbal'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
-def SendEmailq(recipient,amt,name):
-    msg = Message(
-				'BILL AMOUNT',
-				sender ='demo24062000@gmail.com',
-				recipients = [recipient]
-			)
-    SUBJECT = ""
+# def SendEmail(recipient,amt,name):
+#     server = smtplib.SMTP("smtp.gmail.com" , 587)  # 587 = port number
+#     server.ehlo() # check the smtp connection 
+#     server.starttls()  # start the conection 
+#     server.login("demo24062000@gmail.com" , "kvhebbal") 
+#     SUBJECT = "BILL AMOUNT"
+#     amt1=str(amt)
+#     message = 'Subject: {}\n\n{}'.format(SUBJECT,"Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+amt1+" for these many amout of consumption.\n\tYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa")
+#     server.sendmail("demo24062000@gmail.com" , recipient , message)
+#     server.close() 
+
+
+def SendEmail(recipient,amt,name):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "demo24062000@gmail.com"  # Enter your address
+    receiver_email = recipient  # Enter receiver address
+    password = "vbkxwlwyjlpmonuu"
+    SUBJECT = "BILL AMOUNT"
+    # name="Kevin"
     amt1=str(amt)
-    msg.body = "Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+amt1+" for these many amout of consumption.\n\tYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa"
-    mail.send(msg)
+    # message = """\
+    # Subject: Hi there
+
+    # This message is sent from Python."""
+    message = 'Subject: {}\n\n{}'.format(SUBJECT,"Hello "+ name+"\n\tThankyou for using our smart power extender. You will have to pay Rs "+amt1+" for these many amout of consumption.\n\tYou can pay the amout by any of the following ways. \n\tHope you use our extender again. Seeyaa")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+app = Flask(__name__,template_folder='Template')
+
 @app.route("/")
 def billfeed():
     return render_template('bill.html')
@@ -34,7 +47,8 @@ def bill():
     email=request.form['Email']
     hr=request.form['Hrs']
     amt=rate*float(hr)
-#     SendEmailq(email,amt,name)
+    # check()
+    SendEmail(email,amt,name)
     # l={'name':name,'email':email,'hr':hr}
     return render_template('billout.html',name1=name,email1=email,hr1=hr)
     
